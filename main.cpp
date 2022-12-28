e064cd6dea9383744ee19e9238a221a7635344ee
Munish Kumar <send2munish@gmail.com>
2022-12-28 21:12:20 +0530

    Uploaded local project

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <fstream>
+#include <algorithm>
+#include <vector>
+#include <iomanip>
+#include <cctype>
+#include <conio.h>
+#include "core.h"
+
+using namespace std;
+
+int main()
+{
+    loadState();
+    mainMenu();
+    system("cls");
+    cout << "\nHave a great day! Goodbye!\n\n";
+    return 0;
+}
